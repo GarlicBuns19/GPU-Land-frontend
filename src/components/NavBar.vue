ca791812e164d2b809d86773fb804cca2287f819
GarlicBuns19 <planterx26@gmail.com>
2022-08-31 19:55:01 +0100

    Logo 5%

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -2,9 +2,17 @@
 <!-- Navbar -->
   <div>
     <div id="logo">
-      <img src="https://i.postimg.cc/CxHXhbJw/graphics-card-removebg-preview.png" alt="" class="img-fluid">
-    </div>
+      <img src="https://i.postimg.cc/CxHXhbJw/graphics-card-removebg-preview.png" alt="" class="img-fluid" id="logo1">
     <img src="https://i.postimg.cc/jj1c6hh6/logo-fan.png" alt="" class="img-fluid" id="logo2">
+    </div>
+    <div id="main">
+    <div id="photo">
+        <div id="photo-center">
+            <img src="http://blog.jimdo.com/wp-content/uploads/2014/01/tree-247122.jpg" id="large">
+            <img src="http://www.hdwallpapersimages.com/wp-content/uploads/2014/01/Winter-Tiger-Wild-Cat-Images.jpg" id="small">
+        </div>
+    </div>
+</div>
     <button
     class="btn btn-primary"
     type="button"
@@ -110,17 +118,17 @@ export default {
 </script>
 <style scoped>
 #logo{
+  position: relative;
+}
+#logo1{
   width: 100px;
   height: 100px;
-  margin: 0 10px 0 0;
-  position: relative;
+
 }
 #logo2{
   position: absolute;
   width: 42px;
   height: 42px;
-  top: 26px;
-  left: 52.6px;
   animation: rotation 3s infinite linear;
 }
 @keyframes rotation {
@@ -130,5 +138,34 @@ export default {
   to {
     transform: rotate(359deg);
   }
+}
+#main {
+  border: 2px blue solid;
+  width: 100%;
+}
+
+#photo {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+#photo > #photo-center {
+  width: 70%;
+  position: relative;
+  margin: 20px 0px;
+  border: 3px red solid;
+}
+
+#large{
+  width: 100%;
+}
+
+#small{
+  width: 150px;
+  position: absolute;
+  left: 0;
+  top: 0;
+  z-index: 1;
 }
 </style>
