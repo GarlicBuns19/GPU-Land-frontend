64fe83df47ea8f52b58399851f03ae63fadbafd8
GarlicBuns19 <planterx26@gmail.com>
2022-09-07 19:41:33 +0100

    User Profile 99%

--- src/components/SingleUser.vue
+++ src/components/SingleUser.vue
@@ -1,20 +1,97 @@
 <template>
-    <div v-if="user">
-        <h1>{{user[0].user_id}}</h1>
-        <h1>{{user[0].userFName}}</h1>
-        <img :src="user[0].userImg" alt="">
+  <div class="container">
+    <div class="row" v-if="user">
+      <div class="card" style="width: 18rem">
+        <img
+          :src="user[0].userImg"
+          alt="userImg"
+          class="img-fluid card-img-top"
+        />
+        <div class="card-body">
+          <h2>{{ user[0].userFName }} {{ user[0].userLName }}</h2>
+          <h1>{{ user[0].userRole }}</h1>
+        </div>
+        <button
+          id="editUserBtn"
+          type="button"
+          class="btn btn-primary"
+          data-bs-toggle="modal"
+          :data-bs-target="'#updateSUser' + user.user_id"
+        >
+          Edit Your Profile
+        </button>
+      </div>
+      <edituser :user="user" />
     </div>
+    <div v-else>You need to login</div>
+  </div>
 </template>
 <script>
-
+import edituser from "@/components/editSingleUserModal.vue";
 export default {
-    mounted(){
-        this.$store.dispatch('fetchSingleUser',JSON.parse(localStorage.getItem('user')).user_id)
+  mounted() {
+    this.$store.dispatch(
+      "fetchSingleUser",
+      JSON.parse(localStorage.getItem("user")).user_id
+    );
+  },
+  computed: {
+    user() {
+      return this.$store.state.singleUser;
     },
-    computed : {
-        user(){
-            return this.$store.state.singleUser
-        }
-    }
-}
+  },
+  components: {
+    edituser,
+  },
+};
 </script>
+<style scoped>
+.container {
+  height: 51vh;
+}
+.row {
+  margin: 50px 0 0 0;
+  justify-content: center;
+}
+.card {
+  background: rgba(0, 0, 0, 0.144);
+  color: #efefef;
+  border: 3px solid;
+  animation: rgb 2s infinite;
+  border-radius: 30px;
+}
+img {
+  object-fit: cover;
+  aspect-ratio: 1;
+  width: 200px;
+  height: 200px;
+  border-radius: 50%;
+  margin: 10px auto 0 auto;
+  padding: 10px;
+  border: 5px solid;
+  animation: rgb 2s infinite;
+}
+@keyframes rgb {
+  /* 0%  { color: #efefef; } */
+  13.3% {
+    border:5px solid #df40ff;
+  }
+  33.6% {
+    border:5px solid #5b42f3;
+  }
+  54% {
+    border:5px solid #00ddeb;
+  }
+}
+#editUserBtn {
+  margin: 10px;
+  background-image: linear-gradient(144deg, #df40ff, #5b42f3 50%, #00ddeb);
+  color: #efefef;
+  padding: 5px;
+  border-radius: 10px;
+  transition: linear 0.6s;
+}
+#editUserBtn:hover {
+  transform: scale(1.1);
+}
+</style>
---
+++ src/components/editSingleUserModal.vue
@@ -0,0 +1,90 @@
+<template>
+  <div :id="'updateSUser'+ user.user_id" class="modal fade" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">Edit User</h5>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+           <div class="modal-body">
+          <div class="row">
+            <div class="col-md-12 mx-auto">
+             <!-- name -->
+              <div class="mb-3">
+                <label for="Name" class="form-label">User Name:</label>
+                <input
+                  class="form-control"
+                  type="text"
+                  v-model="user[0].userFName"
+                  name="addName"
+                  id="addName"
+                  placeholder="user Name"
+                  required
+                />
+              </div>
+             <!-- surname -->
+              <div class="mb-3">
+                <label for="Name" class="form-label">User surname:</label>
+                <input
+                  class="form-control"
+                  type="text"
+                  v-model="user[0].userLName"
+                  name="addSurnmae"
+                  id="addSurname"
+                  placeholder="user Surname"
+                  required
+                />
+              </div>
+            <!-- email -->
+              <div class="mb-3">
+                <label for="addPrice" class="form-label">Email</label>
+                <input
+                  class="form-control"
+                  type="text"
+                  name="email"
+                  id="email"
+                  v-model="user[0].email"
+                  placeholder="Email"
+                />
+              </div>
+              <!-- Imgae -->
+              <div class="mb-3">
+                <label for="addPrice" class="form-label">Image</label>
+                <input
+                  class="form-control"
+                  type="text"
+                  name="image"
+                  id="image"
+                  v-model="user[0].userImg"
+                  placeholder="Your Profile pic"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">
+              Close
+            </button>
+            <button type="button" class="btn btn-primary" data-bs-dismiss="modal" @click="this.$store.dispatch('editUser',user[0])">
+              Update
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+    props: ["user"],
+    methods: {
+        update() {
+            return this.$store.dispatch("editUser",this.user)
+        }
+    }
+}
+</script>
+<style>
+</style>
